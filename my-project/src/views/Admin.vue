aac7ebf51dd34a43b414ca23f748129b68eace81
Christopher Billy <billy_christopher@hotmail.com>
2021-10-06 21:05:43 +0200

    Ajout structure page Admin

--- my-project/src/views/Admin.vue
+++ my-project/src/views/Admin.vue
@@ -1,41 +1,82 @@
 <template>
   <div class="admin">
-    <ul>
-      <li v-for="(item, index) in items" :key="item.task">
-        <span v-bind:style="styleObject">
-          {{ item.task }}
-        </span>
-        <button @click="color(index)">Editer</button> <button v-on:click="suppr(key)">Supprimer</button>
-      </li>
-    </ul>
-    <input v-model="tasks" placeholder="Ajoutez-moi" />
-    <button v-on:click="add">Ajouter</button>
+    <div class="formulaire m-auto">
+      <form class="d-flex flex-column border border-dark">
+        <div class="d-flex justify-content-between p-3">
+          <div>
+            <label for="author" class="text-left">Auteur :</label>
+            <input type="text" id="author" class="ms-2" placeholder="Nom de l'auteur" />
+          </div>
+          <div>
+            <label for="title">Titre :</label>
+            <input type="text" id="title" class="ms-2" placeholder="Titre du post" />
+          </div>
+          <div>
+            <label for="intro">Intro :</label>
+            <input type="text" id="intro" class="ms-2" placeholder="Intro du post" />
+          </div>
 
+          <button>Ajouter</button>
+        </div>
+
+        <div>
+          <label for="content" class="">Contenu</label>
+          <textarea type="text" id="content" class="ms-2 w-75" placeholder="Contenu du post" />
+        </div>
+      </form>
+    </div>
+
+    <table class="table table-striped mt-4">
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Auteur</th>
+          <th>Date</th>
+          <th>Titre</th>
+          <th>Intro</th>
+          <th>Contenu</th>
+          <th>Modifier | Supprimer</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(article, index) in blogdata.articles" :key="index">
+          <td>{{ index }}</td>
+          <td>{{ article.author }}</td>
+          <td>{{ article.publishedAt | dateFormat }}</td>
+          <td>{{ article.title }}</td>
+          <td>{{ article.description }}</td>
+          <td>{{ article.content }}</td>
+          <td>
+            <button class="me-1">Modifier</button><button>Supprimer</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
 export default {
   name: "Admin",
-  
-  data() {
-    return {
-      items: [{ task: "fo" }, { task: "bar" }],
-      tasks: null,
-    };
-  },
-  methods: {
-
-    suppr(index) {
-      this.items.splice(index, 1);
-    },
-
-    add() {
-      this.items.push({ task: this.tasks });
+  computed: {
+    blogdata() {
+      return this.$store.state.blogdata;
     },
   },
 };
 </script>
 
 <style>
+.admin,
+.formulaire {
+  width: 90%;
+}
+table {
+  table-layout: fixed;
+}
+th,
+td {
+  overflow: auto;
+  white-space: nowrap;
+}
 </style>
--- my-project/src/views/Home.vue
+++ my-project/src/views/Home.vue
@@ -2,9 +2,9 @@
   <div class="home" id="infinite-scroll">
     
     <div class="article border border-3 border-dark rounded m-5" v-for="(article, index) in visibleArticles" :key="index">
-      <h4 class="m-5">{{article.title}}</h4>
-      <p class="m-5">{{article.description}}</p>
-      <p>Publishing date : {{article.publishedAt|dateFormat}}</p>
+      <h4 class="m-5">{{ article.title }}</h4>
+      <p class="m-5">{{ article.description }}</p>
+      <p>Publishing date : {{ article.publishedAt|dateFormat }}</p>
       <router-link :to="`/article/${index}`"><button class="btn btn-dark m-5">Lire +</button></router-link>
     </div>
     <div class="position-absolute">
