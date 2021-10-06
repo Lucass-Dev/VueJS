fde4c3f3e5b38a125d4c85e62881ee94f42ce5cc
Papin Emeric <Papin@MacBook-Pro-de-admin-3.local>
2021-10-06 14:29:49 +0200

    admin

--- my-project/src/router/index.js
+++ my-project/src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import AboutUs from '../views/AboutUs.vue'
 import SinglePost from '../views/SinglePost.vue'
+import Admin from '../views/Admin.vue'
 
 Vue.use(VueRouter)
 
@@ -21,7 +22,12 @@ const routes = [
     path: '/article/:id',
     name: 'SinglePost',
     component: SinglePost
-  }
+  },
+  {
+    path: '/admin',
+    name: 'Admin',
+    component: Admin
+  },
 ]
 
 const router = new VueRouter({
---
+++ my-project/src/views/Admin.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="admin">
+    <ul>
+      <li v-for="(item, index) in items" :key="item.task">
+        <span v-bind:style="styleObject">
+          {{ item.task }}
+        </span>
+        <button @click="color(index)">Editer</button> <button v-on:click="suppr(key)">Supprimer</button>
+      </li>
+    </ul>
+    <input v-model="tasks" placeholder="Ajoutez-moi" />
+    <button v-on:click="add">Ajouter</button>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Admin",
+  
+  data() {
+    return {
+      items: [{ task: "fo" }, { task: "bar" }],
+      tasks: null,
+    };
+  },
+  methods: {
+
+    suppr(index) {
+      this.items.splice(index, 1);
+    },
+
+    add() {
+      this.items.push({ task: this.tasks });
+    },
+  },
+};
+</script>
+
+<style>
+</style>
